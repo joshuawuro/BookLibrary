60183ee914879b1f3d6e99d4b08f369544860af4
Joshuawuro <joshuawuro@gmail.com>
2024-10-16 20:17:37 -0700

    add home import to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./Pages/Home";
 import BookDetails from "./components/BookDetails";
 import PageNotFound from "./pages/PageNotFound";
 import Header from "./components/Header";
